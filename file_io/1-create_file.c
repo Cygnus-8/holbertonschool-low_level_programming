9659a487fbe9d3eab5b8688cee3a72a996b8ef96
Chris <5125@holbertonschool.com>
2022-07-21 18:47:07 +0200

    mode bug fixed

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,11 +15,11 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	if (fd < -1)
 		return (-1);
 
-	while (text_content && *(text_content + len))
+	while (text_content[len] != '\0')
 		len++;
 
 	k = write(fd, text_content, len);

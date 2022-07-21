bb698778c40428aaf5aa78d3270e2de7b34a94bc
Chris <5125@holbertonschool.com>
2022-07-21 18:30:34 +0200

    Functions that create a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,10 +16,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 600);
-	if (fd == -1)
+	if (fd < -1)
 		return (-1);
 
-	while (text_content[len])
+	while (text_content && *(text_content + len))
 		len++;
 
 	k = write(fd, text_content, len);

92e75b54b054868f4f0a581c42690e3e9b0000d5
Chris <5125@holbertonschool.com>
2022-07-21 18:21:08 +0200

    Functions that create a file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_file - cfr description
+ * @filename: the name of the file to create
+ * @text_content: the string to write on filename
+ * Description: Function that create a file and write on
+ * Return: 1 on success or -1 on fail
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, k, len = 0;
+
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 600);
+	if (fd == -1)
+		return (-1);
+
+	while (text_content[len])
+		len++;
+
+	k = write(fd, text_content, len);
+	close(fd);
+
+	if (k == -1)
+		return (-1);
+
+	return (1);
+}

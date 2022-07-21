0d150a838e2e1eef807e7125c6f06782b4c7f717
Chris <5125@holbertonschool.com>
2022-07-21 18:22:59 +0200

    Functions that create a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,6 +12,9 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, k, len = 0;
 
+	if (filename == NULL)
+		return (-1);
+
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 600);
 	if (fd == -1)
 		return (-1);

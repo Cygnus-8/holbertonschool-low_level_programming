7d9ec9082f72ec31ab9a406155bf6fb784cacefc
Chris <5125@holbertonschool.com>
2022-07-22 15:33:07 +0200

    Bug on Read fixed

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,97 +1,91 @@
 #include "main.h"
 
-/**
- * main - main function
- * @ac: argument counter
- * @av: argument vector
- * Return: Always 0
- */
-
+char *custom_buffer(char *file);
 void custom_close(int fd);
-void custom_error(char *filename, int error_code);
-
-int main(int ac, char **av)
-{
-	int i, k, len = 0, fd_from = 0, fd_to = 0;
-	char *file_from, *file_to;
-	char buffer[1024], *tmp;
-
-	if (ac != 3)
-		custom_error("", 97);
-
-	file_from = av[1];
-	file_to = av[2];
-
-	fd_from = open(file_from, O_RDONLY);
-
-	if (fd_from == -1)
-		custom_error(file_from, 98);
-	while (file_from && file_from[len])
-		len++;
-
-	if (len < 1024)
-		i = read(fd_from, buffer, 1024);
-	else
-	{
-		tmp = malloc(sizeof(char) * len);
-		i = read(fd_from, tmp, len);
-	}
-	if (i == -1)
-		custom_error(file_from, 98);
-	custom_close(fd_from);
-	fd_to = open(file_to, O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	if (fd_to == -1)
-		custom_error(file_to, 99);
-	if (len < 1024)
-		k = write(fd_to, buffer, i);
-	else
-		k = write(fd_to, tmp, i);
-	free(tmp);
-	custom_close(fd_to);
-	if (k == -1)
-		custom_error(file_to, 99);
-	return (0);
-}
-/**
- * custom_error - cfr description
- * @error_code: integer error code
- * @filename: file treated
- * Return: Nothing
- */
-
-void custom_error(char *filename, int error_code)
-{
-	if (error_code == 97)
-	{
-		dprintf(2, "Usage: cp file_from file_to%s\n", filename);
-		exit(97);
-	}
-	else if (error_code == 98)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", filename);
-		exit(98);
-	}
-	else
-	{
-		dprintf(2, "Error: Can't write to %s\n", filename);
-		exit(99);
-	}
-}
 
 /**
  * custom_close - cfr description
  * @fd: file descriptor
+ * Description: close the file descriptor
  * Return: Nothing
  */
+
 void custom_close(int fd)
 {
-	int p;
+	int a;
 
-	p = close(fd);
-
-	if (p < 0)
+	a = close(fd);
+	if (a < 0)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
+/**
+ * custom_buffer - cfr description
+ * @file: file to write to
+ * Description: create a buffer
+ * Return: a pointer on buffer if success
+ */
+char *custom_buffer(char *file)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+	if (buffer == NULL)
+	{
+		dprintf(2, "Error: Can't write to %s\n", file);
+		exit(99);
+	}
+	return (buffer);
+}
+/**
+ * main - cfr description
+ * @ac: argument counter
+ * @av: argument vector
+ * Return: Always 0
+ */
+
+int main(int ac, char **av)
+{
+	int i, k, fd_from = 0, fd_to = 0;
+	char *buffer;
+
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	buffer = custom_buffer(av[2]);
+
+	fd_from = open(av[1], O_RDONLY);
+	i = read(fd_from, buffer, 1024);
+	fd_to = open(av[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+
+	do {
+		if (fd_from < 0 || i < 0)
+		{
+			dprintf(2, "Error: Can't read from file %s\n", fd_from);
+			free(buffer);
+			exit(98);
+		}
+		k = write(fd_to, buffer, i);
+		if (fd_to < 0 || k < 0)
+		{
+			dprintf(2, "Error: Can't write to %s\n", fd_to);
+			free(buffer);
+			exit(99);
+		}
+
+		i = read(fd_from, buffer, 1024);
+		fd_to = open(av[2], O_WRONLY | O_APPEND);
+
+	} while (i < 0);
+
+	free(buffer);
+	custom_close(fd_to);
+	custom_close(fd_from);
+
+	return (0);
+}
+

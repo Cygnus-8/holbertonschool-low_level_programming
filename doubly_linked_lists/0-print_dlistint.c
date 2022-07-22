d915f09474c7612dc283dcd4c0ce15a4981ffbfb
Chris <5125@holbertonschool.com>
2022-07-22 16:51:45 +0200

    Task 0

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_dlistint - cfr description
+ * @h: head of the list
+ * Description: Print all elements of a list
+ * Return: The number of nodes printed
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		count++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (count);
+}

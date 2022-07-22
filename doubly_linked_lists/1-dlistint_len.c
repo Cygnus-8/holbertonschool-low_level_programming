1d500d36d932130f31893334eea14e014cf9ce55
Chris <5125@holbertonschool.com>
2022-07-22 16:59:03 +0200

    Task 1

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - cfr description
+ * @h: head of the list
+ * 
+ * Return: The number of element in the list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
